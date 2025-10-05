59ffa7d78341addb2c0d089ddf65a470ce14a3e5
James <qjaymce37@gmail.com>
2025-10-05 20:44:45 +0000

    modify urls.py

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib import admin
+from django.urls import path, include
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,4 +39,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),  # All endpoints live under /api/
 ]
